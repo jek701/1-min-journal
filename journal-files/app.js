6aae18f23df451948a28d217943556b6246945ba
jek701 <ikromhmd@gmail.com>
2021-11-30 18:27:20 +0500

    Added to github

---
+++ journal-files/app.js
@@ -0,0 +1,16 @@
+const HiddenInfoWrapper = document.getElementById('hidden-info-wrapper');
+const HiddenInfo = document.getElementById('hidden-info');
+const HiddenInfoWrapper2 = document.getElementById('hidden-info-wrapper-2');
+const HiddenInfo2 = document.getElementById('hidden-info-2');
+
+HiddenInfoWrapper.onclick = () => {
+    HiddenInfo.classList.toggle('hidden')
+}
+
+HiddenInfoWrapper2.onclick = () => {
+    HiddenInfo2.classList.toggle('hidden')
+}
+
+const HiddenOpen = () => {
+    HiddenInfo2.classList.remove('hidden')
+}
